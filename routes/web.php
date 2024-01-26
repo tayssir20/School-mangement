7acef3f37db7a1914f7804bcff1ae136f0a6280a
tayssir20 <mouhamedtayssir20@gmail.com>
2024-01-26 13:18:21 +0100

    cree resource URL

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::resource('student', StudentController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
