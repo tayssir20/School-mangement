20b5708ab087d0846e50623daa623ef5e10f8472
tayssir20 <mouhamedtayssir20@gmail.com>
2024-01-26 12:23:33 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends ('layout')
+@section('content')
+    <table border="0" style="width: 800px ; margin:50px;">
+        <tr>
+            <td>
+                <img src="img/student.jpg" width="175" height="175">
+            </td>
+             <td>
+                <img src="img/teacher.png" width="175" height="175">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <img src="img/library.png" width="175" height="175">
+            </td>
+             <td>
+                <img src="img/administrator.png" width="175" height="175">
+            </td>
+        </tr>
+
+    </table>
+@endsection
