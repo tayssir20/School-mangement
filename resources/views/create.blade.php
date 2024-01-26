ad4fe49da4e36cae582bd629323d11a430938a54
tayssir20 <mouhamedtayssir20@gmail.com>
2024-01-26 15:47:18 +0100

    creation d'un formulaire et protection les champs

---
+++ resources/views/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+@section('content')
+<h3>Add student</h3>
+<form methode="post" action="{{route('student.store')}}" enctype="multipart/form-data">
+    @csrf
+<div class="form-groupe">
+    <label for="name">Name:</label>
+    <input type="text" class="form-control" name="name"/>
+</div>
+
+<div class="form-groupe">
+    <label for="name">Email:</label>
+    <input type="text" class="form-control" name="email"/>
+</div>
+
+
+<div class="form-groupe">
+    <label for="name">Phone:</label>
+    <input type="text" class="form-control" name="phone"/>
+</div>
+
+
+
+<div class="form-groupe">
+    <label for="name">Section</label>
+    <input type="text" class="form-control" name="section"/>
+</div>
+
+
+<div class="form-groupe">
+    <label for="name">Image:</label>
+    <input type="file" class="form-control" name="image"/>
+</div>
+
+
+<button type="submit" class="btn btn-primary">Add Student </button>
+</form>
+@endsection
