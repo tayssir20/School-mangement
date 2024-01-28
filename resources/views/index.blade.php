4547b4702953407b5bd37875d0a2cb523e8c7595
tayssir20 <mouhamedtayssir20@gmail.com>
2024-01-28 16:14:28 +0100

    affichage list des etudiant

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,30 @@
 @extends ('layout')
 @section('content')
     <h3> Student list</h3>
+    <div class="table-wrapper">
+        <table class="fl-table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Section</th>
+                    <th>Image</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $student)
+                    <tr>
+                        <td>{{ $student->id }}</td>
+                        <td>{{ $student->name }}</td>
+                        <td>{{ $student->email }}</td>
+                        <td>{{ $student->phone }}</td>
+                        <td>{{ $student->section }}</td>
+                        <td><img src="/image/"{{ $student->image }} width="96" height="96"/></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
